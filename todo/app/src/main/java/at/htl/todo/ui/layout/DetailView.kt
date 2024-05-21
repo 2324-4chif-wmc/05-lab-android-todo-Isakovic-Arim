297377bfc783bb5b7b95cb189995893408c91745
Isakovic-Arim <arim.isakovic2005@gmail.com>
2024-05-21 19:18:55 +0200

    Update the Card Design in DetailView.kt

--- todo/app/src/main/java/at/htl/todo/ui/layout/DetailView.kt
+++ todo/app/src/main/java/at/htl/todo/ui/layout/DetailView.kt
@@ -1,14 +1,20 @@
 package at.htl.todo.ui.layout
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -33,7 +39,6 @@ fun Details(model: Model, toDoService: TodoService?, store: ModelStore?) {
 @Composable
 fun TodoCard(todo: Todo) {
     Card(
-        onClick = {todo.completed = !todo.completed},
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceVariant,
         ),
@@ -47,11 +52,22 @@ fun TodoCard(todo: Todo) {
                 .padding(16.dp),
             textAlign = TextAlign.Center
         )
-        Text(
-            text = "completed: " + todo.completed,
+        Row(
             modifier = Modifier
-                .padding(16.dp),
-            textAlign = TextAlign.Center
-        )
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "completed: ",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Checkbox(
+                checked = todo.completed,
+                onCheckedChange = { /* Update the completed status of the todo item */ }
+            )
+        }
     }
 }
